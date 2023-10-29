7b30e1168e8a85a61601e71ed442ec03f469fc0f
James <jamesvollmer02@gmail.com>
2023-10-29 15:58:09 -0500

    Add date/time selection and verification

--- app/src/main/java/com/cs407/wakeguard/AlarmEditorActivity.java
+++ app/src/main/java/com/cs407/wakeguard/AlarmEditorActivity.java
@@ -18,12 +18,21 @@ import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.ToggleButton;
 
+
 public class AlarmEditorActivity extends AppCompatActivity {
-    TimePicker picker;
+    TimePicker tPicker;
+    // TextView showing selected alarm date or repeating days
     TextView selectedDateText;
-    int year;
-    int month;
-    int dayOfMonth;
+    // Whether or not the alarm will repeat on one or more days
+    private boolean repeating;
+    // Date chosen in DatePicker (for convenience with scrolling between times today vs tomorrow)
+    private Calendar dpDate;
+    // Actual alarm date
+    int year, month, dayOfMonth;
+    // Strings for selectedDateText when repeating on one or more days
+    private final String[] dayOfWeekStrings = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
+    // Days on which the alarm repeats: Sun    Mon    Tue    Wed    Thu    Fri    Sat
+    private boolean[] repeatingDays = {false, false, false, false, false, false, false};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +42,7 @@ public class AlarmEditorActivity extends AppCompatActivity {
         // Get 24-hour mode from SharedPreferences. Use it below to set time picker 24 hour mode
         SharedPreferences sp = getSharedPreferences("com.cs407.wakeguard", Context.MODE_PRIVATE);
         Boolean mode24Hr = sp.getBoolean("24hourMode", false);
+
 
         // TODO Test alarm tone
         //Uri ringtoneUri  = RingtoneManager.getActualDefaultRingtoneUri(getApplicationContext(), RingtoneManager.TYPE_NOTIFICATION);
@@ -44,38 +54,151 @@ public class AlarmEditorActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
 
-        // Get the current (or existing alarm's) year, month, and day of month and set dateText accordingly
-        Calendar c = Calendar.getInstance();
-        year = intent.getIntExtra("year", c.get(Calendar.YEAR));
-        month = intent.getIntExtra("month", c.get(Calendar.MONTH));
-        dayOfMonth = intent.getIntExtra("dayOfMonth", c.get(Calendar.DAY_OF_MONTH));
+        // Set time picker to show 6am or the existing alarm's time
+        tPicker = (TimePicker) findViewById(R.id.timePicker);
+        tPicker.setHour(intent.getIntExtra("hour", 6));
+        tPicker.setMinute(intent.getIntExtra("minute", 0));
+        tPicker.setIs24HourView(mode24Hr);
 
-        // TODO Initialize specific/repeating date(s) in SharedPreferences/database (if not already initialized/passed in)
-
-        selectedDateText = (TextView) findViewById(R.id.dateText);
-        String dateStr = DateFormat.getDateInstance(DateFormat.FULL).format(c.getTime());
-        if((year == c.get(Calendar.YEAR)) &&
-           (month == c.get(Calendar.MONTH)) &&
-           (dayOfMonth == c.get(Calendar.DAY_OF_MONTH))) {
-            selectedDateText.setText("Today - " + dateStr);
-        } else {
-            selectedDateText.setText(dateStr);
+        // If an existing alarm was clicked on the home page,
+        // repeated days will be sent to this activity via an Intent
+        boolean[] tempRepeatingDays = intent.getBooleanArrayExtra("repeatingDays");
+        repeating = false;
+        // Check if intent extra with repeating days was sent
+        if(tempRepeatingDays != null) {
+            // Check for at least one repeating day
+            for(int i=0; i < tempRepeatingDays.length; i++) {
+                repeatingDays[i] = tempRepeatingDays[i];
+                if(tempRepeatingDays[i]) {
+                    repeating = true;
+                }
+            }
         }
 
-        // Set time picker to show 6am or the existing alarm's time
-        picker = (TimePicker) findViewById(R.id.timePicker);
-        picker.setHour(intent.getIntExtra("hour", 6));
-        picker.setMinute(intent.getIntExtra("minute", 0));
-        picker.setIs24HourView(mode24Hr);
+        selectedDateText = (TextView) findViewById(R.id.dateText);
+        if(repeating) {
+            // Repeating at least one day. Set dateText and buttons for repeating days accordingly
+            selectedDateText.setText(getRepeatingDaysString());
+            ((ToggleButton)findViewById(R.id.sundayBtn)).setChecked(repeatingDays[0]);
+            ((ToggleButton)findViewById(R.id.mondayBtn)).setChecked(repeatingDays[1]);
+            ((ToggleButton)findViewById(R.id.tuesdayBtn)).setChecked(repeatingDays[2]);
+            ((ToggleButton)findViewById(R.id.wednesdayBtn)).setChecked(repeatingDays[3]);
+            ((ToggleButton)findViewById(R.id.thursdayBtn)).setChecked(repeatingDays[4]);
+            ((ToggleButton)findViewById(R.id.fridayBtn)).setChecked(repeatingDays[5]);
+            ((ToggleButton)findViewById(R.id.saturdayBtn)).setChecked(repeatingDays[6]);
+        } else {
+            // Not repeating any days. Get the existing alarm's/current year, month, and day of month
+            // and set dateText accordingly
+            Calendar c = Calendar.getInstance();
+            year = intent.getIntExtra("year", c.get(Calendar.YEAR));
+            month = intent.getIntExtra("month", c.get(Calendar.MONTH));
+            dayOfMonth = intent.getIntExtra("dayOfMonth", c.get(Calendar.DAY_OF_MONTH));
+            // Set DatePicker date against current alarm date
+            // Allows for cycling between today and tomorrow using TimePicker initially
+            dpDate = Calendar.getInstance();
+            dpDate.set(Calendar.YEAR, year);
+            dpDate.set(Calendar.MONTH, month);
+            dpDate.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+            // Validate the displayed/used date for the alarm
+            c = getValidDate(year, month, dayOfMonth);
+            String dateStr = DateFormat.getDateInstance(DateFormat.FULL).format(c.getTime());
+            if((year == c.get(Calendar.YEAR)) &&
+               (month == c.get(Calendar.MONTH)) &&
+               (dayOfMonth == c.get(Calendar.DAY_OF_MONTH))) {
+                selectedDateText.setText("Today - " + dateStr);
+            } else {
+                selectedDateText.setText(dateStr);
+            }
+        }
 
-        // TODO Use setOnTimeChangedListener() to listen for time changes and move the date to tomorrow if the chosen time is less than the current time
-        // TODO Should also check the time differences when saving the alarm so that the date is changed if the chosen time ends up being sooner than the current time (e.g. if the user chooses a minute past the current time and then sets the alarm 2 minutes later)
+        // Set TimePicker onTimeChangedListener
+        tPicker.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
+            @Override
+            public void onTimeChanged(TimePicker view, int hourOfDay, int minute) {
+                Calendar c = Calendar.getInstance();
+                // Check that there are no repeating days and that DatePicker selected date is today
+                if(!repeating && dpDate.compareTo(c) <= 0) {
+                    // Get valid date (today or tomorrow) according to the current time and chosen alarm time
+                    c = getValidDate(dpDate.get(Calendar.YEAR), dpDate.get(Calendar.MONTH), dpDate.get(Calendar.DAY_OF_MONTH));
+                    year = c.get(Calendar.YEAR);
+                    month = c.get(Calendar.MONTH);
+                    dayOfMonth = c.get(Calendar.DAY_OF_MONTH);
+                    // Check if returned Calendar points to today
+                    if(c.compareTo(Calendar.getInstance()) <= 0) {
+                        // Setting alarm for today
+                        selectedDateText.setText("Today - " + DateFormat.getDateInstance(DateFormat.FULL).format(c.getTime()));
+                    } else {
+                        // Setting alarm for later date
+                        selectedDateText.setText(DateFormat.getDateInstance(DateFormat.FULL).format(c.getTime()));
+                    }
+                }
+            }
+        });
+    }
 
-        // TODO Use picker.getMinute() to get currently selected minute
-        // TODO Use picker.getHour() to get currently selected hour
+    private String getRepeatingDaysString() {
+        String repeatingDaysStr = "Every";
+        int repCount = 0;
+
+        // Add repeating days to the string
+        for(int i=0; i < repeatingDays.length; i++) {
+            if(repeatingDays[i]) {
+                // Add commas before second and later repeating days
+                if(repCount > 0) {
+                    repeatingDaysStr += ",";
+                }
+                repeatingDaysStr += " " + dayOfWeekStrings[i];
+                repCount++;
+            }
+        }
+
+        if(repCount == dayOfWeekStrings.length) {
+            return "Every day";
+        }
+        return repeatingDaysStr;
+    }
+
+    private Calendar getValidDate(int y, int m, int dom) {
+        Calendar c = Calendar.getInstance();
+
+        if((y == c.get(Calendar.YEAR)) &&
+           (m == c.get(Calendar.MONTH)) &&
+           (dom == c.get(Calendar.DAY_OF_MONTH))) {
+            // Passed date is today
+            final int actualTimeOfDay = c.get(Calendar.HOUR_OF_DAY) * 60 + c.get(Calendar.MINUTE);
+            final int selectedTimeOfDay = tPicker.getHour() * 60 + tPicker.getMinute();
+
+            c.set(Calendar.YEAR, y);
+            c.set(Calendar.MONTH, m);
+            c.set(Calendar.DAY_OF_MONTH, dom);
+
+            // Only allow the date to be set for the current day if the selected alarm
+            // time is less than the actual time
+            if(actualTimeOfDay >= selectedTimeOfDay) {
+                // Change date to tomorrow
+                c.roll(Calendar.DAY_OF_YEAR, true); // TODO Does this roll properly, or do I have to update all other fields?
+            }
+        } else {
+            c.set(Calendar.YEAR, y);
+            c.set(Calendar.MONTH, m);
+            c.set(Calendar.DAY_OF_MONTH, dom);
+        }
+
+        return c;
     }
 
     public void openDatePicker(View v) {
+        // Validate previously chosen date date before creating DatePickerDialog
+        Calendar c = getValidDate(dpDate.get(Calendar.YEAR), dpDate.get(Calendar.MONTH), dpDate.get(Calendar.DAY_OF_MONTH));
+        year = c.get(Calendar.YEAR);
+        month = c.get(Calendar.MONTH);
+        dayOfMonth = c.get(Calendar.DAY_OF_MONTH);
+
+        // If time has skewed enough to change the valid date to tomorrow, update dateText
+        if(!repeating && (c.compareTo(Calendar.getInstance()) > 0)) {
+            selectedDateText.setText(DateFormat.getDateInstance(DateFormat.FULL).format(c.getTime()));
+        }
+
         // Create DatePickerDialog
         DatePickerDialog dpDialog = new DatePickerDialog(
                 this,
@@ -83,25 +206,38 @@ public class AlarmEditorActivity extends AppCompatActivity {
                 new DatePickerDialog.OnDateSetListener() {
                     @Override
                     public void onDateSet(DatePicker dp, int y, int m, int dom) {
-                        Calendar tempCal = Calendar.getInstance();
-                        Boolean today = (y == tempCal.get(Calendar.YEAR)) &&
-                                        (m == tempCal.get(Calendar.MONTH)) &&
-                                        (dom == tempCal.get(Calendar.DAY_OF_MONTH));
-                        tempCal.set(Calendar.YEAR, y);
-                        tempCal.set(Calendar.MONTH, m);
-                        tempCal.set(Calendar.DAY_OF_MONTH, dom);
-                        year = y;
-                        month = m;
-                        dayOfMonth = dom;
-                        String dateStr = DateFormat.getDateInstance(DateFormat.FULL).format(tempCal.getTime());
+                        // Set DatePicker date to actually selected date
+                        dpDate = Calendar.getInstance();
+                        dpDate.set(Calendar.YEAR, y);
+                        dpDate.set(Calendar.MONTH, m);
+                        dpDate.set(Calendar.DAY_OF_MONTH, dom);
+                        // Check date according to selected alarm time
+                        Calendar c = getValidDate(y, m, dom);
 
-                        // TODO Set SharedPreferences/database alarm date (overwrite repeating days in SharedPreferences)
-
-                        if(today) {
-                            selectedDateText.setText("Today - " + dateStr);
+                        year = c.get(Calendar.YEAR);
+                        month = c.get(Calendar.MONTH);
+                        dayOfMonth = c.get(Calendar.DAY_OF_MONTH);
+                        // Check if returned Calendar points to today
+                        if(c.compareTo(Calendar.getInstance()) <= 0) {
+                            // Setting alarm for today
+                            selectedDateText.setText("Today - " + DateFormat.getDateInstance(DateFormat.FULL).format(c.getTime()));
                         } else {
-                            selectedDateText.setText(dateStr);
+                            // Setting alarm for later date
+                            selectedDateText.setText(DateFormat.getDateInstance(DateFormat.FULL).format(c.getTime()));
                         }
+
+                        // Reset repeating days
+                        repeating = false;
+                        for(int i=0; i < repeatingDays.length; i++) {
+                            repeatingDays[i] = false;
+                        }
+                        ((ToggleButton)findViewById(R.id.sundayBtn)).setChecked(repeatingDays[0]);
+                        ((ToggleButton)findViewById(R.id.mondayBtn)).setChecked(repeatingDays[1]);
+                        ((ToggleButton)findViewById(R.id.tuesdayBtn)).setChecked(repeatingDays[2]);
+                        ((ToggleButton)findViewById(R.id.wednesdayBtn)).setChecked(repeatingDays[3]);
+                        ((ToggleButton)findViewById(R.id.thursdayBtn)).setChecked(repeatingDays[4]);
+                        ((ToggleButton)findViewById(R.id.fridayBtn)).setChecked(repeatingDays[5]);
+                        ((ToggleButton)findViewById(R.id.saturdayBtn)).setChecked(repeatingDays[6]);
                     }
                 },
                 year,
@@ -117,28 +253,52 @@ public class AlarmEditorActivity extends AppCompatActivity {
     }
 
     public void setRepeatingDay(View v) {
-        SharedPreferences sp = getSharedPreferences("com.cs407.wakeguard", Context.MODE_PRIVATE);
-
         if(v.getId() == R.id.sundayBtn) {
-            sp.edit().putBoolean("repeatSunday", ((ToggleButton)v).isChecked()).apply(); // TODO Add to/update database instead
+            repeatingDays[0] = ((ToggleButton)v).isChecked();
         } else if(v.getId() == R.id.mondayBtn) {
-            sp.edit().putBoolean("repeatMonday", ((ToggleButton)v).isChecked()).apply(); // TODO Add to/update database instead
+            repeatingDays[1] = ((ToggleButton)v).isChecked();
         } else if(v.getId() == R.id.tuesdayBtn) {
-            sp.edit().putBoolean("repeatTuesday", ((ToggleButton)v).isChecked()).apply(); // TODO Add to/update database instead
+            repeatingDays[2] = ((ToggleButton)v).isChecked();
         } else if(v.getId() == R.id.wednesdayBtn) {
-            sp.edit().putBoolean("repeatWednesday", ((ToggleButton)v).isChecked()).apply(); // TODO Add to/update database instead
+            repeatingDays[3] = ((ToggleButton)v).isChecked();
         } else if(v.getId() == R.id.thursdayBtn) {
-            sp.edit().putBoolean("repeatThursday", ((ToggleButton)v).isChecked()).apply(); // TODO Add to/update database instead
+            repeatingDays[4] = ((ToggleButton)v).isChecked();
         } else if(v.getId() == R.id.fridayBtn) {
-            sp.edit().putBoolean("repeatFriday", ((ToggleButton)v).isChecked()).apply(); // TODO Add to/update database instead
+            repeatingDays[5] = ((ToggleButton)v).isChecked();
         } else if(v.getId() == R.id.saturdayBtn) {
-            sp.edit().putBoolean("repeatSaturday", ((ToggleButton)v).isChecked()).apply(); // TODO Add to/update database instead
+            repeatingDays[6] = ((ToggleButton)v).isChecked();
         } else {
             return;
         }
 
-        // TODO If repeating on one or more days,
-        //        - Remove specific alarm date in SharedPreferences/database
-        //        - Change TextView text to call out repeated days or go back to current day/tomorrow (based on time) if this action disabled the last repeating day
+        // Check for at least one repeating day
+        repeating = false;
+        for(int i=0; i < repeatingDays.length; i++) {
+            if(repeatingDays[i]) {
+                repeating = true;
+                break;
+            }
+        }
+
+        // Reset date to today (or tomorrow if alarm is set too early for today)
+        // This is to reset the DatePicker selection
+        dpDate = Calendar.getInstance();
+        Calendar c = getValidDate(dpDate.get(Calendar.YEAR), dpDate.get(Calendar.MONTH), dpDate.get(Calendar.DAY_OF_MONTH));
+        year = c.get(Calendar.YEAR);
+        month = c.get(Calendar.MONTH);
+        dayOfMonth = c.get(Calendar.DAY_OF_MONTH);
+        if(repeating) {
+            // Set dateText to repeating days
+            selectedDateText.setText(getRepeatingDaysString());
+        } else {
+            // Check if returned Calendar points to today
+            if(c.compareTo(Calendar.getInstance()) <= 0) {
+                // Setting alarm for today
+                selectedDateText.setText("Today - " + DateFormat.getDateInstance(DateFormat.FULL).format(c.getTime()));
+            } else {
+                // Setting alarm for tomorrow
+                selectedDateText.setText(DateFormat.getDateInstance(DateFormat.FULL).format(c.getTime()));
+            }
+        }
     }
 }

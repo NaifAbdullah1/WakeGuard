4cfd47d9b85f8a3d83ae7b5cbcf025b116bf2430
Nathan Abdullah <nabdullah2@wisc.edu>
2023-11-03 18:45:40 -0500

    Dashboard's clock added

--- app/src/main/java/com/cs407/wakeguard/DashboardActivity.java
+++ app/src/main/java/com/cs407/wakeguard/DashboardActivity.java
@@ -4,11 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.arbelkilani.clock.Clock;
+import com.arbelkilani.clock.enumeration.ClockType;
+
 public class DashboardActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        // Getting the object representing the dashboard's clock
+        Clock dashboardClock = findViewById(R.id.dashboardClock);
+
+        // Setting the clock type to numeric (instead of analog)
+        dashboardClock.setClockType(ClockType.numeric);
+
+        dashboardClock.setPadding(0, 0, 0, 0);
     }
 }

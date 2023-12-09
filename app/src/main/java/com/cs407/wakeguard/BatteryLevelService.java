d9df078c7b0f5753b24ce6d7aea181a3ee0471a5
ryskoski <vryskoski@wisc.edu>
2023-12-08 20:09:56 -0600

    Low Battery Alarm Warning Alert

--- app/src/main/java/com/cs407/wakeguard/BatteryLevelService.java
+++ app/src/main/java/com/cs407/wakeguard/BatteryLevelService.java
@@ -1,15 +1,21 @@
 package com.cs407.wakeguard;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.SharedPreferences;
 import android.os.BatteryManager;
+import android.os.Build;
 import android.os.IBinder;
+import android.provider.Settings;
 
 public class BatteryLevelService extends Service {
     private BroadcastReceiver batteryLevelReceiver;
+    private DBHelper dbHelper= DBHelper.getInstance(this);
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         batteryLevelReceiver = new BroadcastReceiver() {
@@ -18,7 +24,12 @@ public class BatteryLevelService extends Service {
                 int level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
                 int scale = intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
                 float batteryPct = level * 100 / (float)scale;
-                System.out.println(batteryPct);
+                if (batteryPct <= 69) {
+                    // Schedule Alarm
+                    AlarmCard warningAlarm = new AlarmCard("WARNING!", "", "Battery Is Less Than 69%",
+                            "Default", true, false, true);
+                    scheduleAlarm(warningAlarm);
+                }
             }
         };
 
@@ -37,6 +48,53 @@ public class BatteryLevelService extends Service {
     public IBinder onBind(Intent intent) {
         // Used only in case of bound services.
         return null;
+    }
+
+    private void scheduleAlarm (AlarmCard alarmCard){
+        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        // This intent is sent to AlarmReceiver.java with the alarm's ID as an extra
+        Intent alarmReceiverIntent = new Intent(this, AlarmReceiver.class);
+        alarmReceiverIntent.putExtra("alarmId", alarmCard.getId());
+
+        long immediateAlarmTime = System.currentTimeMillis();
+
+        // Generate a unique request code
+        int requestCode = generateRequestCode();
+        dbHelper.addRequestCode(alarmCard.getTitle()+alarmCard.getTime()+alarmCard.get12HrTime()+"ALERT", requestCode, "");
+
+        // Use the unique request code for the PendingIntent
+        PendingIntent alarmPendingIntent = PendingIntent.getBroadcast(this, requestCode,
+                alarmReceiverIntent, PendingIntent.FLAG_IMMUTABLE);
+
+        // Set the alarm to go off immediately
+        if (alarmManager != null) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                if (alarmManager.canScheduleExactAlarms()) {
+                    alarmManager.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, immediateAlarmTime, alarmPendingIntent);
+                } else {
+                    // Show a dialog or notification to inform the user they need to grant the permission
+                    Intent permissionIntent = new Intent(Settings.ACTION_REQUEST_SCHEDULE_EXACT_ALARM);
+                    startActivity(permissionIntent);
+                }
+            } else {
+                // For older versions, set the alarm without checking the permission
+                alarmManager.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, immediateAlarmTime, alarmPendingIntent);
+            }
+        }
+    }
+
+    private synchronized int generateRequestCode(){
+        int reqCode = DashboardActivity.requestCodeCreator++;
+        saveRequestCode(DashboardActivity.requestCodeCreator); // saving it to SharedPreference for persistence.
+        return reqCode;
+    }
+
+    private void saveRequestCode(int requestCode){
+        SharedPreferences sharedPreferences = getSharedPreferences("com.cs407.wakeguard",
+                Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putInt("nextRequestCode", DashboardActivity.requestCodeCreator);
+        editor.apply();
     }
 }
 
